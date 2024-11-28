efd453081753274a11642f72aa13ef3dd0450aa0
Jatin Poriya <111553605+InterestingPlus@users.noreply.github.com>
2024-11-28 16:52:54 +0530

    Added Search Bar

--- Frontend/src/components/admin/patient/Book.js
+++ Frontend/src/components/admin/patient/Book.js
@@ -18,35 +18,40 @@ function AllDoctors() {
   if (doctors) {
     return (
       <>
-        <h1>
-          <i class="fa-solid fa-user-doctor"></i> Select Doctor to Book
-          Appointment
+        <h1 id="list-heading">
+          <b>
+            <i class="fa-solid fa-user-doctor"></i> Select Doctor
+          </b>{" "}
+          to <br /> Book Appointment
         </h1>
 
+        <section id="search-box">
+          <div>
+            <input type="text" />
+            <button>
+              <i class="fi fi-br-search"></i>
+            </button>
+          </div>
+        </section>
+
         <ul id="all-doctors">
-          {doctors.data.data.map((doctor, index) => {
+          {doctors?.data?.data?.map((doctor, index) => {
             return (
-              <li key={index}>
-                <img
-                  src="https://img.freepik.com/premium-vector/doctor-woman-smiling-profile-cartoon_18591-60679.jpg"
-                  alt="profile-pic"
-                />
-                <h1>{doctor.name}</h1>
-                <h2>{doctor.specialization}</h2>
-                <h3>{doctor.availability}</h3>
-                <div>
-                  <Link
-                    to={`/patient-dashboard/appointments/doctor/${doctor.username}`}
-                  >
-                    <button>View Detail</button>
-                  </Link>
-                  <Link
-                    to={`/patient-dashboard/appointments/book-next/${doctor.username}`}
-                  >
-                    <button className="aptmnt">Book Appointment</button>
-                  </Link>
-                </div>
-              </li>
+              <Link
+                to={`/patient-dashboard/appointments/doctor/${doctor.username}`}
+              >
+                <li key={index}>
+                  <img
+                    src="https://img.freepik.com/premium-vector/doctor-woman-smiling-profile-cartoon_18591-60679.jpg"
+                    alt="profile-pic"
+                  />
+                  <span>
+                    <h1>{doctor.name}</h1>
+                    <h2>{doctor.specialization}</h2>
+                    <h3>{doctor.availability}</h3>
+                  </span>
+                </li>
+              </Link>
             );
           })}
         </ul>
