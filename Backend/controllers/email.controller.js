e1c3fcbd0341b262c01f10b5e33e3c164cf9102b
Jatin Poriya <111553605+InterestingPlus@users.noreply.github.com>
2025-01-04 09:48:53 +0530

    Backend Added

--- Backend/controllers/email.controller.js
+++ Backend/controllers/email.controller.js
@@ -65,6 +65,8 @@ module.exports.otpVerification = async (req, res) => {
         `,
     });
 
+    console.log("Email Sent!", info);
+
     // Respond with the OTP
     res.status(200).json({ success: true, otp });
   } catch (error) {
@@ -127,6 +129,7 @@ module.exports.notifyDoctor = async (
     </div>
       `,
     });
+
     console.log(`Notification Sent to the Doctor : ${doctorName}`);
   } catch (error) {
     console.log("Error sending email: ", error);
