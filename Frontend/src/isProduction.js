7e50ad9d1f9d3411805a484a893a57610eeccd1e
Jatin Poriya <111553605+InterestingPlus@users.noreply.github.com>
2025-01-04 14:53:22 +0530

    Update isProduction.js

--- Frontend/src/isProduction.js
+++ Frontend/src/isProduction.js
@@ -1,18 +1,21 @@
 async function apiPath() {
   let isProduction = false;
+  let url = "";
 
   await fetch("/production.json")
     .then((response) => response.json())
     .then((data) => {
       isProduction = data.production;
+      url = data.url;
     })
     .catch((error) => {
       alert("Internet Not Connected!");
     });
 
   if (isProduction) {
-    return "https://hms-backend-production-7d9d.up.railway.app";
+    // return "https://hms-backend-production-7d9d.up.railway.app";
     // return "https://hms-backend-tr2u.onrender.com";
+    return url;
   } else {
     return "http://localhost:4444";
     // return "http://192.168.129.169:4444";
